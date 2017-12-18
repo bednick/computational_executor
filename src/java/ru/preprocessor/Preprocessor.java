618d2f3cf31034f620c3bb52cab986e7346f0950
Bedarev N.A <79139978241@mail.ru>
2017-12-18 20:35:32 +0700

    add preprocess

--- src/java/ru/preprocessor/Preprocessor.java
+++ src/java/ru/preprocessor/Preprocessor.java
@@ -6,13 +6,16 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Preprocessor implements IPreprocessor {
-    private final String START_COMMENT = "##";
-    private final String EXTEND_LINE = "\\";
     private final String EXTENSION_IN = ".cm";
     private final String EXTENSION_OUT = ".cmc";
+
+    private StringBuilder builder = new StringBuilder();
+    private String nextLine;
 
     public Preprocessor() {}
 
@@ -28,14 +31,22 @@ public class Preprocessor implements IPreprocessor {
     public File process(File file) throws IOException {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8));
-        String line;
         List<String> lines = new ArrayList<String>();
-        while ((line = readLine(reader)) != null) {
-            line = line.trim();
-            if (line.isEmpty() || line.startsWith(START_COMMENT)) {
+        readLine(reader);
+        while (nextLine != null) {
+            if (nextLine.isEmpty() || nextLine.startsWith("##")) {
                 continue;
             }
-            lines.add(line);
+            if (nextLine.startsWith("#")) {
+                if (nextLine.startsWith("#define")) {
+                    lines.addAll(readFor(reader));
+                }
+                if (nextLine.startsWith("#for")) {
+                    lines.addAll(readFor(reader));
+                }
+            } else {
+                lines.add(getCorrectLine(nextLine));
+            }
         }
         if (!lines.isEmpty()) {
             Files.write(Paths.get(getNewName(file.getName())), lines, StandardOpenOption.CREATE);
@@ -44,27 +55,60 @@ public class Preprocessor implements IPreprocessor {
         // throw file is empty
         return null;
     }
-    private StringBuilder builder = new StringBuilder();
 
-    private String readLine(BufferedReader reader) throws IOException {
+    private void readLine(BufferedReader reader) throws IOException {
         String line;
-        while ((line = readLine(reader)) != null) {
-            if (line.endsWith(EXTEND_LINE)) {
+        while ((line = reader.readLine()) != null) {
+            if (line.endsWith("\\")) {
                 builder.append(line);
                 builder.deleteCharAt(builder.length()-1);
+                builder.append(' ');
             } else {
                 if (builder.length() > 0) {
+                    builder.append(line);
                     line = builder.toString();
-                    builder.deleteCharAt(builder.length()-1);
+                    builder.delete(0, builder.length());
                 }
-                return line;
+                nextLine = line.trim();
+                return;
             }
         }
         if (builder.length() > 0) {
             line = builder.toString();
             builder.deleteCharAt(builder.length()-1);
         }
-        return line;
+        if (line != null){
+            line = line.trim();
+        }
+        nextLine = line;
+    }
+
+    private List<String> readFor(BufferedReader reader) throws IOException {
+        ArrayList<String> lines = new ArrayList<String>();
+        while (!nextLine.startsWith("#endfor")) {
+            readLine(reader);
+            lines.add(getCorrectLine(nextLine));
+        }
+        readLine(reader);
+        return lines;
+    }
+
+    private String getCorrectLine(String line) throws IOException {
+        // TODO parsing for ... in
+        String in = String.join(" ", Arrays.stream(line.split(";")[0].split(" "))
+                .filter(l->!l.isEmpty()).collect(Collectors.toList()));
+        String out = String.join(" ",Arrays.stream(line.split(";")[1].split(" "))
+                .filter(l->!l.isEmpty()).collect(Collectors.toList()));
+
+        List<String> com_and_labels = Arrays.asList(line.split(";")[2].split("#"));
+        String com = com_and_labels.get(0).trim();
+        String label = "#";
+        if (com_and_labels.size() > 1) {
+            label = String.join(" ", Arrays.asList(com_and_labels.get(1).split(" ")));
+        }
+        String rez = builder.append(in).append(';').append(out).append(';').append(com).append(label).toString();
+        builder.delete(0, builder.length());
+        return rez;
     }
 
 }
